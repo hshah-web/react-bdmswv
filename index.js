ca1ddc2edbdf7e75569699c56d661f4491dc99a7
hshah-web <shah.harsheel@gmail.com>
2020-03-24 11:39:14 -0400

    3 files modified

--- index.js
+++ index.js
@@ -2,25 +2,45 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import Header from "./common-components/header-component";
+import Footer from "./common-components/footer-component";
+import { browserHistory, Router, Route, Switch } from 'react-router';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+const routing = (
+  <Router>
+    <div>
+      <Header />
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+);
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+ReactDOM.render(routing, document.getElementById("root"));
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: 'React'
+//     };
+//   }
 
-render(<App />, document.getElementById('root'));
+//   render() {
+//     return (
+//       <div>
+//         <Hello name={this.state.name} />
+//         <p>
+//           Start editing to see some magic happen :)
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// render(<App />, document.getElementById('root'));
